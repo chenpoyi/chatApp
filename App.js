fc0be76d8994a84d50f63203e4b3626cc3f2a308
Paul Chen <paulchen647@gmail.com>
2021-07-26 19:47:24 -0700

    Add firebase.

--- App.js
+++ App.js
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import 'react-native-gesture-handler';
 
@@ -13,13 +6,7 @@ import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from './Components/HomeScreen';
 
 const Stack = createStackNavigator();
 
